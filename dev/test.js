275fd44413a06ec8eba330ca519869a2f6ae4e56
Russ Carroll <info@rcarroll.dev>
2021-09-18 23:44:34 -0400

    Completed initial section.

--- dev/test.js
+++ dev/test.js
@@ -2,23 +2,4 @@ const Blockchain = require('./blockchain');
 
 const testcoin = new Blockchain();
 
-const previouseBlockHash = 'adfadfasfda';
-const currentBlockData = [
-  {
-    amount: 1,
-    sender: 'bob',
-    recipient: 'tom',
-  },
-  {
-    amount: 10,
-    sender: 'bob',
-    recipient: 'tom',
-  },
-  {
-    amount: 100,
-    sender: 'bob',
-    recipient: 'tom',
-  },
-];
-
-console.log(testcoin.hashBlock(previouseBlockHash, currentBlockData, 18606));
+console.log(testcoin);
